3d663d9cdfc7ef8abab4c79fd1b2c646de27fe9e
zaylinalcorte11223 <60014102+zaylinalcorte11223@users.noreply.github.com>
2020-01-23 12:36:15 -0600

    makes desicions
    decisions

---
+++ sketch.js
@@ -0,0 +1,35 @@
+// References found at p5js.org
+
+// Add your variables here
+var r = 50;
+var g = 150;
+var b = 190;
+var rectY = 0;
+
+
+// initialize your program here
+function setup() {
+    createCanvas(500, 500);
+    
+}
+
+
+
+
+// Essentially a repeat command
+function draw() {
+
+
+for (var i=0; i < 15; i++){
+    rect(20, 20, 60, 60);
+rect(0, rectY, 500, 500);
+  rectY += 500/15;
+  r += 14;
+  b -= 10;
+ 
+}
+fill(255);
+square(30, 20, 55);
+    
+}
+ 
